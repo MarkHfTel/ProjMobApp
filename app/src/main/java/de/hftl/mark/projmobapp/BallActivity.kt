4eb5b9b212c82944c141734c8b79abe48a851b86
Mark <s168235@hft-leipzig.de>
2019-01-07 12:59:08 +0100

    Update

--- app/src/main/java/de/hftl/mark/projmobapp/BallActivity.kt
+++ app/src/main/java/de/hftl/mark/projmobapp/BallActivity.kt
@@ -3,10 +3,15 @@ package de.hftl.mark.projmobapp
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.webkit.WebView
 
 class BallActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ball)
+
+        var wv = findViewById<WebView>(R.id.wv_ball)
+        wv.settings.javaScriptEnabled = true
+        wv.loadUrl("file:///android_assets/Magic8Ball.html")
     }
 }
